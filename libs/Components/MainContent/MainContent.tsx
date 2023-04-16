bbc0338aa68466c862189906c2fb922afbba4b38
Sukanta Das <arnobh84@gmail.com>
2023-04-16 15:49:29 +0600

    handleAi function added

--- libs/Components/MainContent/MainContent.tsx
+++ libs/Components/MainContent/MainContent.tsx
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 import InputField from "../InputField/InputField";
 import Image from "next/image";
 import ChatMessage from "../ChatMessage/ChatMessage";
+import { BACKEND_BASE_URL } from "@/libs/config/const";
 
 const MainContent = () => {
   const [input, setInput] = useState<any>("");
   const [chatLog, setChatLog] = useState<any>([
-    { user: "gpt", message: "how can i Help You" },
+    { user: "gpt", message: "hy Cristain How can i help you" },
+    { user: "me", message: "i want to use chatGPT-2.0" },
   ]);
 
   const handleCallAi = async () => {
     console.log(input);
     setChatLog([...chatLog, { user: "me", message: `${input}` }]);
     setInput("");
+
+    const response = await fetch(`${BACKEND_BASE_URL}/api/v1/ai/ask`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        message: chatLog.map((message: any) => message?.message).join(""),
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
   };
 
   return (
