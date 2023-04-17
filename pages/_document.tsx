d238d37fec3a6f95b8fcd132d69737bfe5853472
Sukanta Das <arnobh84@gmail.com>
2023-04-17 11:53:09 +0600

    titile added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link
-        href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
-        rel="stylesheet"
-      ></link>
+      <Head>
+        <title>chatGPT-2.0</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
